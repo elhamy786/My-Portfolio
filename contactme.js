cf0231fb0dc26b24bb14b10b4b23baa62a67c836
elhamy786 <elham.afzali1383@gmail.com>
2024-08-10 01:14:29 +0430

    Edit contactme.js file

--- contactme.js
+++ contactme.js
@@ -1,28 +1,28 @@
 function submitForm() {
   const firstName = document.getElementById('firstName').value.trim();
   const lastName = document.getElementById('lastName').value.trim();
-  const company = document.getElementById('company').value.trim();
   const email = document.getElementById('email').value.trim();
   const phone = document.getElementById('phone').value.trim();
   const message = document.getElementById('message').value.trim();
+
   if (!firstName || !lastName || !email || !message) {
-    alert('Please fill out all required fields.');
+    alert('Please fill out all required fields.');// eslint-disable-next-line no-alert
     return;
-}
-const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-if (!emailPattern.test(email)) {
-  alert('Please enter a valid email address.');
-  return;
-}
-const phonePattern = /^\d+$/;
-if (!phonePattern.test(phone)) {
-  alert('Please enter a valid phone number (digits only).');
-  return;
-}
-const form = document.getElementById('contactForm');
-  form.action = "https://formspree.io/f/xblrodky";
-  form.method = "POST";
+  }
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    alert('Please enter a valid email address.');// eslint-disable-next-line no-alert
+    return;
+  }
+  const phonePattern = /^\d+$/;
+  if (!phonePattern.test(phone)) {
+    alert('Please enter a valid phone number (digits only).');// eslint-disable-next-line no-alert
+    return;
+  }
+  const form = document.getElementById('contactForm');
+  form.action = 'https://formspree.io/f/xblrodky';
+  form.method = 'POST';
   form.submit();
-alert('Your message has been sent!');
+  alert('Your message has been sent!');// eslint-disable-next-line no-alert
   form.reset();
 }
